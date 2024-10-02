706f353e42901e50820994b0998c1c7e65fa8dce
Leonardo Montero <leonardo.tmontero@telefonica.com>
2024-10-01 21:16:31 -0300

    feat: add exmples

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,6 +20,10 @@ const config: Config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  themes: ['@docusaurus/theme-mermaid'],
+  markdown: {
+    mermaid: true,
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
